48ec4fc5d6ad41656fc9f39ebdcee18090ec5254
Rafid Al Razi <rafidalrazi@gmail.com>
2024-04-06 03:06:10 +0600

    2nd commit.

--- registration/registration/urls.py
+++ registration/registration/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 # from django.contrib import admin
 from django.urls import path
 from app1 import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.SignupPage,name='signup'),
-    path('login/',views.LoginPage,name='login'),
-    path('home/',views.HomePage,name='home'),
-    path('logout/',views.LogoutPage,name='logout'),
+    path('', views.SignupPage, name='signup'),
+    path('login/', views.LoginPage, name='login'),
+    path('home/', views.HomePage, name='home'),
+    path('logout/', views.LogoutPage, name='logout'),
+    path('user/', views.UserPage, name='user'),
 
-
-    
 ]
