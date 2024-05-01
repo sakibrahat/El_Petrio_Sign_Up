4e9b8ee98391e0034444d6dc0863aaa81ce2792a
NazifaKhushbu <bhuiyannazifa2002@gmail.com>
2024-05-01 19:44:58 +0600

    4th commit

--- registration/app1/forms.py
+++ registration/app1/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class SearchForm(forms.Form):
     query = forms.CharField(label='Search')
+class PetForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ['name', 'price']
+
